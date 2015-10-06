4e5688a52d800f4a5b4088e77f2b42973a91901e
Key-F <kirill2010.97@mail.ru>
2015-10-07 01:00:24 +0300

    works

--- palindrom/palindrom/Program.cs
+++ palindrom/palindrom/Program.cs
@@ -8,19 +8,44 @@ namespace palindrom
 {
     class Program
     {
+        public static bool ofPal(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
+            return true;
+        }
+
         public static bool Palindrom(string s)
         {
             string s1 = s;
-            bool k = false;
-            for (int i = 0; i < s.Length / 2; i++)
-                if (s[i] != s[s.Length - i - 1])
-                    k = true;  
-            if (k == true)
+            string s2 = null;
+            string s3;
+            int maxpol = 0;
+            bool trigger = true;
+            if (ofPal(s) == false)
+            for (int i = 0; i < s.Length; i++)
             {
-                for (int i = 1; i < s.Length ; i++)
-                    s1 += s[s.Length - i - 1];
+                int ll = i;
+                s3 = s2 + s[s.Length - i - 1]; // рассматриваем с конца строки
+                if (ofPal(s3) == false)
+                    trigger = false;  // уже не палиндром
+                else
+                {
+                    if (ll > maxpol) // поиск длины максималлного палиндрома
+                        maxpol = s2.Length + 1; // +1 из-за сдвига индекса и длины
+                }
+                
+                s2 += s[s.Length - i - 1]; // добавляем еще один символ для проверки на палиндром
             }
-            Console.WriteLine(s1);
+            if (trigger == false)
+            for (int i1 = 0; i1 < s.Length - maxpol; i1++)
+            {
+                s1 += s[s.Length - maxpol - i1 - 1];
+            }
+            Console.WriteLine(s1);   
             return true;
         }
 
