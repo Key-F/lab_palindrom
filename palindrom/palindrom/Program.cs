33dd0ec65059609e1416c91fee1538b06d6cc978
Key-F <kirill2010.97@mail.ru>
2015-10-06 11:35:46 +0300

    done

---
+++ palindrom/palindrom/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    class Program
+    {
+        public static bool Palindrom(string s)
+        {
+            string s1 = s;
+            bool k = false;
+            for (int i = 0; i < s.Length / 2; i++)
+                if (s[i] != s[s.Length - i - 1])
+                    k = true;  
+            if (k == true)
+            {
+                for (int i = 1; i < s.Length ; i++)
+                    s1 += s[s.Length - i - 1];
+            }
+            Console.WriteLine(s1);
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string s =  Console.ReadLine();
+            Palindrom(s);
+            Console.ReadLine();
+        }
+    }
+}
